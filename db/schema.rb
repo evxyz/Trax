fa6fbd30dd24a3a1fba0e7142022d5bf75e68131
evx001 <eviexeris@yahoo.com>
2012-05-12 18:52:33 -0400

    paperclip add audio to track model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120512222859) do
+ActiveRecord::Schema.define(:version => 20120512224747) do
 
   create_table "tracks", :force => true do |t|
     t.string   "songs"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "audio_file_name"
+    t.string   "audio_content_type"
+    t.integer  "audio_file_size"
+    t.datetime "audio_updated_at"
   end
 
 end
