f6514ee7eff3dd98235103d40195d28406c0fb07
evx001 <eviexeris@yahoo.com>
2012-05-12 18:04:17 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trax::Application.config.secret_token = '3911b72855ffc8c06da2ac0c956e0a74a4537a6d7d12d3c885c34e17707556915e6f9849415beb97e41ba3d08843b23329ecc7ba956707c3545372cc42a74b42'
